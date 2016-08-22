9f640567ee75ebf769909dad734b60772ec505bb
zhuxiankui <zhuxiankui@zhuxiankuis-MacBook-Pro.local>
2016-08-22 17:08:09 +0800

    chapter05: Avoiding Array Mutations

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 /**
- * chapter04: React Counter Example
+ * chapter05: Avoiding Array Mutations
  */
 
 import React from 'react';
@@ -9,13 +9,34 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
 // a reducer
-const counter = (state = 0, action) => {
-	console.log('counter reducer action', action)
+// const counter = (state = [1, 3, 5], action) => {
+// 	Object.freeze(state);
+// 	let len = state.length;
+// 	switch (action.type) {
+// 		case 'PUSH':
+// 			state.push(len*2 + 1)
+// 			return state;
+// 		case 'SPLICE':
+// 			state.splice(len - 1, 1);
+// 			return state;
+// 		default: 
+// 			return state;
+// 	}
+// }
+
+const counter = (state = [1, 3, 5], action) => {
+	Object.freeze(state); // this is default, because of state track and virtue dom diff
+	let len = state.length;
 	switch (action.type) {
-		case 'INCREMENT':
-			return state + 1;
-		case 'DECREMENT':
-			return state - 1;
+		case 'PUSH':
+			return [
+				...state,
+				len * 2 + 1,
+			];
+		case 'SPLICE':
+			return [
+				...state.slice(0, len-1)
+			];
 		default: 
 			return state;
 	}
@@ -29,26 +50,29 @@ const Counter = ({
   value,
   onIncrement,
   onDecrement
-}) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
+}) => {
+	console.log('component render ***')
+	return (
+	  <div>
+	    <h1>{value}</h1>
+	    <button onClick={onIncrement}>+</button>
+	    <button onClick={onDecrement}>-</button>
+	  </div>
+	);
+}
 
 const render = () => {
   ReactDOM.render(
     <Counter
-      value={store.getState()}
+      value={store.getState().join(', ')}
       onIncrement={() =>
         store.dispatch({
-          type: 'INCREMENT'
+          type: 'PUSH'
         })
       }
       onDecrement={() =>
         store.dispatch({
-          type: 'DECREMENT'
+          type: 'SPLICE'
         })
       }
     />,
